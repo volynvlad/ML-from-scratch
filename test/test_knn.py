2cfa3d3864de09a2567400b829a765f632860b62
volynvlad <volynvlad@gmail.com>
2022-02-19 15:25:04 +0300

    Change data declaration place

--- test/test_knn.py
+++ test/test_knn.py
@@ -2,30 +2,14 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-# import matplotlib.pyplot as plt
-# from matplotlib.colors import ListedColormap
-
 from algorithms.knn import KNN
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=300)
 
 
 def test_knn():
-
-    # cmap = ListedColormap(["red", "green", "blue"])
-
-    iris = datasets.load_iris()
-    X, y = iris.data, iris.target
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=300)
-
-    # print(X_train.shape)
-    # print(y_train.shape)
-
-    # print(X_train[0])
-    # print(y_train[0])
-
-    # plt.figure()
-    # plt.scatter(X[:, 2], X[:, 3], c=y, cmap=cmap, edgecolors='k', s=20)
-    # plt.show()
 
     ks = [3, 5, 7]
     ps = [1, 2, 3]
